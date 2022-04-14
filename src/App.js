f09ada6d9c2ca0c5c84ca202b5e4511e09236616
Brijisha Doshi <brijishadoshi12@gmail.com>
2022-04-15 00:31:41 +0530

    Enable daak mode-up to video 12

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 // import logo from './logo.svg';
 // import './App.css';
 // import About from "./components/About";
+import { useState } from "react";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 
 function App() {
+  const [mode, setMode] = useState("light"); //Whether Dark mode is enable or not
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+    }
+  };
   return (
     <>
-      <Navbar title="TextUtils" aboutText="About TextUtils" />
+      <Navbar
+        title="TextUtils"
+        aboutText="About TextUtils"
+        mode={mode}
+        toggleMode={toggleMode}
+      />
       <div className="container my-3">
-        <TextForm heading="Enter your text to analyze below" />
+        <TextForm heading="Enter your text to analyze below" mode={mode} />
         {/* <About /> */}
       </div>
     </>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -21,7 +21,7 @@ export default function TextForm(props) {
   //This is a function to make every first letter of word in capital
   const capitalFirstLetter = () => {
     let words = text.split(" ");
-    let uppercaseword = " ";
+    let uppercaseword = "";
     words.forEach((element) => {
       uppercaseword += element.charAt(0).toUpperCase() + element.slice(1) + " ";
     });
@@ -32,12 +32,35 @@ export default function TextForm(props) {
     setText("");
   };
 
+  const handleCopy = () => {
+    let text = document.getElementById("mybox");
+    text.select();
+    navigator.clipboard
+      .writeText(text.value)
+      .then(() => {
+        alert("successfully copied");
+      })
+      .catch(() => {
+        alert("something went wrong");
+      });
+  };
+
+  const handleExtraSpaces = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+  };
+
   const [text, setText] = useState("");
   // text = "new text"; //wrong way to change the text
   // setText("new text"); //Correct way to change the text
   return (
     <>
-      <div className="container">
+      <div
+        className="container"
+        style={{
+          color: props.mode === "light" ? "black" : "white",
+        }}
+      >
         <h1>{props.heading}</h1>
         <div className="mb-5">
           <textarea
@@ -46,6 +69,10 @@ export default function TextForm(props) {
             onChange={handleOnChange}
             id="mybox"
             rows="10"
+            style={{
+              backgroundColor: props.mode === "light" ? "white" : "#042743",
+              color: props.mode === "light" ? "black" : "white",
+            }}
           ></textarea>
         </div>
         <button className="btn btn-primary mx-1" onClick={handleUpClick}>
@@ -60,15 +87,29 @@ export default function TextForm(props) {
         <button className="btn btn-primary mx-1" onClick={clearText}>
           Clear Text
         </button>
+        <button className="btn btn-primary mx-1 my-3" onClick={handleCopy}>
+          Copy Text
+        </button>
+        <button
+          className="btn btn-primary mx-1 my-3"
+          onClick={handleExtraSpaces}
+        >
+          Remove Extra Spaces
+        </button>
       </div>
-      <div className="container">
+      <div
+        className="container"
+        style={{
+          color: props.mode === "light" ? "black" : "white",
+        }}
+      >
         <h2>Your text summary</h2>
         <p>
           {text.split(" ").length} words and {text.length} characters
         </p>
         <p>{0.008 * text.split(" ").length} Minutes read</p>
         <h2>Preview</h2>
-        <p>{text}</p>
+        <p>{text || "Enter your text to preview"}</p>
       </div>
     </>
   );
