c6aef673c0c146f5c7d3659f9fa21d4e8a5ad5e5
Brijisha <brijishadoshi12@gmail.com>
2022-04-18 11:39:38 +0530

    Updated Final TextUtils

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -25,64 +25,16 @@ export default function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/">
+              <Link className="nav-link" aria-current="page" to="/">
                 Home
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link active" to="/about">
+              <Link className="nav-link" to="/about">
                 {props.aboutText}
               </Link>
             </li>
-            {/* <li className="nav-item dropdown">
-              <Link
-                className="nav-link dropdown-toggle"
-                to="/"
-                id="navbarDropdown"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Dropdown
-              </Link>
-              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li>
-                  <Link className="dropdown-item" to="/">
-                    Action
-                  </Link>
-                </li>
-                <li>
-                  <Link className="dropdown-item" to="/">
-                    Another action
-                  </Link>
-                </li>
-                <li>
-                  <hr className="dropdown-divider" />
-                </li>
-                <li>
-                  <Link className="dropdown-item" to="/">
-                    Something else here
-                  </Link>
-                </li>
-              </ul>
-            </li> 
-            <li className="nav-item">
-              <Link className="nav-link disabled" to="/">
-                Disabled
-              </Link>
-            </li>*/}
           </ul>
-          {/* <form className="d-flex">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button className="btn btn-outline-primary" type="submit">
-              Search
-            </button>
-          </form> */}
           <div
             className={`form-check form-switch text-${
               props.mode === "light" ? "dark" : "light"
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,21 +2,18 @@ import React, { useState } from "react";
 
 export default function TextForm(props) {
   const handleUpClick = () => {
-    //console.log("Upper case was clicked" + text);
     let newText = text.toUpperCase();
     setText(newText);
     props.showAlert("Converted to uppercase", "success");
   };
 
   const handleLoClick = () => {
-    //console.log("Upper case was clicked" + text);
     let newText = text.toLowerCase();
     setText(newText);
     props.showAlert("Converted to lowercase", "success");
   };
 
   const handleOnChange = (event) => {
-    // console.log("On Change");
     setText(event.target.value);
   };
 
@@ -40,9 +37,10 @@ export default function TextForm(props) {
   };
 
   const handleCopy = () => {
-    let text = document.getElementById("mybox");
-    text.select();
-    navigator.clipboard.writeText(text.value);
+    // let text = document.getElementById("mybox");
+    // text.select();
+    // navigator.clipboard.writeText(text.value);
+    navigator.clipboard.writeText(text);
     // .then(() => {
     //   alert("successfully copied");
     // })
@@ -137,7 +135,7 @@ export default function TextForm(props) {
         <strong>
           <p>
             {
-              text.split(" ").filter((element) => {
+              text.split(/\s+/).filter((element) => {
                 return element.length !== 0;
               }).length
             }{" "}
